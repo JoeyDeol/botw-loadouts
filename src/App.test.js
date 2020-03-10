7e7aab01a4bb7b803f53b39af1c533c5efabb48b
Joey Deol <gurjothdeol@gmail.com>
2020-03-10 14:17:28 -0400

    Complete initial project setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test('App should render without crashing.', () =>{
+  const container = document.createElement('div');
+
+  ReactDOM.render(<App />, container);
+})
