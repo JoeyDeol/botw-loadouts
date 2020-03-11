aaa0d415c2ea1c6a922a07e99bd58b1c9df1e7f3
Joey Deol <gurjothdeol@gmail.com>
2020-03-11 11:03:24 -0400

    Create img element for logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ function Header() {
   return(
     <header>
       <h1>Breath of the Wild Loadouts</h1>
+      <img src="./assets/zelda-botw-logo.png" alt="Zelda Breath of the Wild Logo"/>
       <h2>Manage your Gear</h2>
       <h2>Focus on your Adventure</h2>
     </header>
