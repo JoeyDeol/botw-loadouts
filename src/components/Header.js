957a33df99550625f646aa2544880af4040dbac7
Joey Deol <gurjothdeol@gmail.com>
2020-03-11 14:11:41 -0400

    Create background-assets folder and start Header initial styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,12 @@ import React from 'react';
 function Header() {
   return(
     <header>
-      <h1>Breath of the Wild Loadouts</h1>
-      <img src="./assets/zelda-botw-logo.png" alt="Zelda Breath of the Wild Logo"/>
-      <h2>Manage your Gear</h2>
-      <h2>Focus on your Adventure</h2>
+      <div className="wrapper">
+        <h1>Breath of the Wild Loadouts</h1>
+        <img src="./assets/zelda-botw-logo.png" alt="Zelda Breath of the Wild Logo"/>
+        <h2>Manage your Gear</h2>
+        <h2>Focus on your Adventure</h2>
+      </div>
     </header>
   )
 }
